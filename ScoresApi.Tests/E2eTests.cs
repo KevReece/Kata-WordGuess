da90f357dd0e36c347005d94dc143ec5d9df2bcd
Kev Reece <kevinjamesreece@gmail.com>
2024-08-06 15:21:47 +0200

    redis integration

--- ScoresApi.Tests/E2eTests.cs
+++ ScoresApi.Tests/E2eTests.cs
@@ -15,21 +15,22 @@ public class E2eTests
     }
     
     [Fact]
-    public async Task GetTopScoresEndpointReturnsScore()
-    {
-        var client = new ScoresApiClient();
-        var response = await client.GetTopScoresAsync();
-
-        var responseText = await response.Content.ReadAsStringAsync();
-        Assert.Contains("value", responseText);
-    }
-    
-    [Fact]
     public async Task PostScoreEndpointReturnsSuccess()
     {
         var client = new ScoresApiClient();
         var response = await client.PostScoreAsync("Test", 10);
         
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task GetTopScoresEndpointReturnsScore()
+    {
+        var client = new ScoresApiClient();
+        await client.PostScoreAsync("Test", 10);
+        var response = await client.GetTopScoresAsync();
+
+        var responseText = await response.Content.ReadAsStringAsync();
+        Assert.Contains("value", responseText);
     }
 }
--- ScoresApi/ScoresRepository.cs
+++ ScoresApi/ScoresRepository.cs
@@ -1,21 +1,38 @@
+using System.Text.Json;
+using StackExchange.Redis;
+
 namespace ScoresApi;
 
 public class ScoresRepository
 {
+    private readonly IDatabase _database;
+    
+    public ScoresRepository()
+    {
+        var redisConnectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING") ?? "redis:6379";
+        var redis = ConnectionMultiplexer.Connect(redisConnectionString);
+        _database = redis.GetDatabase();
+    }
+
     public IEnumerable<Score> GetTopScores()
     {
-        return Enumerable.Range(1, 3).Select(index =>
-            new Score
-                (
-                    DateTime.Now.AddDays(-2),
-                    "ABC",
-                    10
-                ))
-            .OrderByDescending(score => score.Value)
-            .Take(10);
+        var scores = _database.SortedSetRangeByRankWithScores("scores", 0, 9, Order.Descending);
+        return scores.Select(DeserialiseEntry);
+    }
+
+    private static Score DeserialiseEntry(SortedSetEntry entry)
+    {
+        if (entry.Element.IsNull)
+        {
+            throw new InvalidOperationException("Null entry in scores");
+        }
+        return JsonSerializer.Deserialize<Score>(entry.Element!) 
+            ?? throw new InvalidOperationException("Failed to deserialise score");
     }
 
     public void AddScore(Score score)
     {
+        var scoreJson = JsonSerializer.Serialize(score);
+        _database.SortedSetAdd("scores", scoreJson, score.Value);
     }
 }
